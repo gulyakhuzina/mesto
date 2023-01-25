dc02f4a92023d31ce37b248029a15b7d82dfde0a
Gulya Khuzina <khuzinagulya1506@yandex.ru>
2023-01-25 23:39:17 +0500

    Настроить webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
+import '../pages/index.css';
+import { initialCards } from './constants.js';
 import { Card } from './Card.js';
 import { FormValidator, config } from './FormValidator.js';
 import { Section } from './Section.js';
